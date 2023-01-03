ee9490ccdb2485fc2b17aacdf9036041f3472030
Erick Galdamez <vasquez.galdamez@outlook.fr>
2023-01-03 18:09:19 +0100

    ajout CSS

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nouveau Site Perso</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/style.css">
+  <title>Nouveau Site Web</title>
 </head>
 <body>
-    <h1>Creation du Nouveau Site</h1>
-    <?php
+<h1>Acceuil</h1>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa dolore quos porro est doloremque aliquid ratione excepturi sunt non! Laborum optio ratione et voluptates fugit numquam asperiores ad labore eum?</p>
 
-
-?>
-    
 </body>
 </html>
